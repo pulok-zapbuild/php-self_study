3ba04f611d9049da8bfd11dbcd5f79a109555993
pulok-zapbuild <pulok.zapbuild@gmail.com>
2020-01-21 18:34:07 +0530

    calculator php file

--- calculator.php
+++ calculator.php
@@ -1,71 +1,48 @@
 <?php
-
-
-class Calci
-{
-	
-	function add($a,$b)#to add two variable
-	{
-		echo $a+$b;# code...
-	}
-	function sub($a,$b)#to subtract two variable
-	{
-		echo $a-$b;
-	}
-	function mul($a,$b)#to multiply two variable
-	{
-		echo $a*$b;
-	}
-	function div($a,$b)#to perform the division
-	{
-		echo $a/$b;
-	}
-	function root($a)#to find the root of a variable
-	{
-		$b=sqrt($a);
-		echo $b;
-	}
-}
-#craeting the object of class"calci"
-$obj=new Calci();
+function __autoload($var)#function defined
+    {
+        require_once $var.".php";
+    }
 
 #here we are getting the value form by post method
 $first = $_POST['first'];
 $second = $_POST['second'];
 $op = $_POST["op"];
-$r="&#8730";
-echo "<center>";
 
-echo "result is ";
+#craeting the object of class"calci" which calls the autoload function to load the Claci.php file
+$obj=new Calci();
+
+echo "<center>";
 switch ($op) {
 	case '+':
 	#calling the add() function to perform the addition
-		$obj->add($first,$second);
+		$r=$obj->add($first,$second);
+		echo $r;
 		break;
 	case '-':
 	#calling the sub() function to perform the subtraction
-		$obj->sub($first,$second);# code...
+		$r=$obj->sub($first,$second);# code...
+		echo $r;
 		break;
 	case '*':
 	#calling the mul() function to perform the multiplication
-		$obj->mul($first,$second);# code...
+		$r=$obj->mul($first,$second);# code...
+		echo $r;
 		break;
 	case '/':
 	#calling the div() function to perform the division
-		$obj->div($first,$second);# code...
+		$r=$obj->div($first,$second);# code...
+		echo $r;
 		break;
 	case 'rt':
 	#calling the root() function to perform the square root
-		$obj->root($first);
+		$obj->root($first,$second);
 		break;
 	default:
 		 echo acb;# code...
 		break;
 	
 }
-
-echo "</center>";
-
 ?>
 <br><br><br>
-<center><a href="calculator.html">go to calculator</a></center>
+<center><a href="calculator.html">Go to Calculator</a></center>
