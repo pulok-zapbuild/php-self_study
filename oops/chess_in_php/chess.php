9829b1727a062c741175ade9d2a671a9ae8a2de6
pulok-zapbuild <pulok.zapbuild@gmail.com>
2020-01-24 16:49:31 +0530

    chess class php file

---
+++ oops/chess_in_php/chess.php
@@ -0,0 +1,16 @@
+<?php
+	class chess
+	{
+		public function black()#to print the black part
+		{
+			echo '<font color ="black" size="6">W</font>';
+		}
+		
+		public function white()#to print the white part
+		{
+			echo '<font color="white" size="6">W</font>';
+		}
+
+	}
+
+?>
